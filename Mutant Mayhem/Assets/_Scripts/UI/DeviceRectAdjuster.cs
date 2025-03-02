96c42342eea6d4fb32405446e2f98f34bbc0cbc4
AiryShelf <matt.j.vanalstyne@gmail.com>
2025-03-01 23:08:54 -0800

    Missed Changes

--- Mutant Mayhem/Assets/_Scripts/UI/DeviceRectAdjuster.cs
+++ Mutant Mayhem/Assets/_Scripts/UI/DeviceRectAdjuster.cs
@@ -21,7 +21,7 @@ public class DeviceRectAdjuster : MonoBehaviour
     [SerializeField] Vector2 cutomScalePivot = new Vector2(0.5f, 0.5f);
 
     [Header("Positions Adjustments")]
-    [SerializeField] Vector2 widePositionOffest = Vector2.zero;
+    [SerializeField] Vector2 defaultWidePositionOffest = Vector2.zero;
     [SerializeField] Vector2 narrowPositionOffset = Vector2.zero;
 
     private void OnValidate()
@@ -98,19 +98,19 @@ public class DeviceRectAdjuster : MonoBehaviour
         float aspectRatio = (float)Screen.width / Screen.height;
         if (aspectRatio >= 2.0f)
         {
-            rectToScale.anchoredPosition = widePositionOffest;
+            rectToScale.anchoredPosition = defaultWidePositionOffest;
         }
         else if (aspectRatio > 1.85f)
         {
-            rectToScale.anchoredPosition = widePositionOffest;
+            rectToScale.anchoredPosition = defaultWidePositionOffest;
         }
         else if (aspectRatio >= 1.77f)
         {
-            rectToScale.anchoredPosition = startAnchoredPos;
+            rectToScale.anchoredPosition = defaultWidePositionOffest;
         }
         else if (aspectRatio > 1.5f) 
         {
-            rectToScale.anchoredPosition = startAnchoredPos;
+            rectToScale.anchoredPosition = startAnchoredPos + narrowPositionOffset;
         }
         else
         {
